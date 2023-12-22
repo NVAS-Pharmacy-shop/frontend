9ff9b83ee7c9a149f70c4f3beb24b03aa7512358
seli0001 <nikolasehovac@gmail.com>
2023-12-22 04:33:49 +0100

    nesto

--- nvasshop/src/components/UserProfileComponent/UserProfileComponent.tsx
+++ nvasshop/src/components/UserProfileComponent/UserProfileComponent.tsx
@@ -1,84 +1,110 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react";
 import AuthContext from "../../context/AuthContext";
 import { Button, Grid, TextField } from "@mui/material";
 import exp from "constants";
-import "./UserProfileComponent.css"
+import "./UserProfileComponent.css";
 import { User, getUser } from "../../service/https/user-service";
 import api from "../../api";
 
 const UserProfileComponent = () => {
-    const { user } = useContext(AuthContext);
-    const [userData, setUser] = useState<User|null>(null);
-    const [first_name, setFirstName] = useState<string>("");
-    const [last_name, setLastName] = useState<string>("");
+  const { user } = useContext(AuthContext);
+  const [userData, setUser] = useState<User | null>(null);
+  const [first_name, setFirstName] = useState<string>("");
+  const [last_name, setLastName] = useState<string>("");
 
-    useEffect(() => {
-        if (user) {
-            api.get(`/user/${user.user_id}`)
-            .then((response) => {
-                setFirstName(response.data.user.first_name);
-                setLastName(response.data.user.last_name);
-                setUser(response.data.user);
-            })
-            .catch((error) => {
-                console.error("Error fetching user", error);
-            });
-        }
-    }, [user]);
-
-    const updateUser = () => {
-        const userDataNew = {
-            first_name: first_name,
-            last_name: last_name,
-        }
-        api.put(`/user/${user?.user_id}/`, userDataNew)
+  useEffect(() => {
+    if (user) {
+      api
+        .get(`/user/${user.user_id}`)
         .then((response) => {
-            setUser(response.data.user);
+          setFirstName(response.data.user.first_name);
+          setLastName(response.data.user.last_name);
+          setUser(response.data.user);
         })
         .catch((error) => {
-            console.error("Error fetching user", error);
+          console.error("Error fetching user", error);
         });
     }
+  }, [user]);
 
-    if (userData === null) {
-        return (
-            <Grid className="card">
-                <Grid
-                    container
-                    direction="column"
-                    justifyContent="center"
-                    alignItems="center"
-                    >
-                    <h1>User Profile</h1>
-                    <TextField variant="outlined" disabled value="Loading..."></TextField>
-                    <TextField variant="outlined" disabled value="Loading..."></TextField>
-                    <TextField variant="outlined" disabled value="Loading..."></TextField>
-                    <TextField variant="outlined" disabled value="Loading..."></TextField>
-                    </Grid>
-            </Grid>
-        );
-    }
+  const updateUser = () => {
+    const userDataNew = {
+      first_name: first_name,
+      last_name: last_name,
+    };
+    api
+      .put(`/user/${user?.user_id}/`, userDataNew)
+      .then((response) => {
+        setUser(response.data.user);
+      })
+      .catch((error) => {
+        console.error("Error fetching user", error);
+      });
+  };
+
+  if (userData === null) {
     return (
-        <Grid className="card">
-            <Grid
-                container
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-                >
-                <h1>User Profile</h1>
-                <TextField sx={{width: "300px", marginBottom: "10px"}} variant="outlined" disabled value={userData!.email}></TextField>
-                <TextField sx={{width: "300px", marginBottom: "10px"}} variant="outlined" value={first_name} onChange={(event) => {
-                    setFirstName(event.target.value);
-                 }}></TextField>
-                <TextField sx={{width: "300px", marginBottom: "10px"}} variant="outlined" value={last_name} onChange={(event) => {
-                    setLastName(event.target.value);
-                }}></TextField>
-                <TextField sx={{width: "300px", marginBottom: "10px"}} variant="outlined" disabled value={userData!.role}></TextField>
-                <Button variant="contained" color="primary" onClick={updateUser}>Update</Button>
-                </Grid>
+      <Grid className="card">
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <h1>User Profile</h1>
+          <TextField variant="outlined" disabled value="Loading..."></TextField>
+          <TextField variant="outlined" disabled value="Loading..."></TextField>
+          <TextField variant="outlined" disabled value="Loading..."></TextField>
+          <TextField variant="outlined" disabled value="Loading..."></TextField>
         </Grid>
+      </Grid>
     );
-}
+  }
+  return (
+    <Grid className="card">
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <h1>User Profile</h1>
+        <TextField
+          sx={{ width: "300px", marginBottom: "10px" }}
+          variant="outlined"
+          disabled
+          value={userData!.email}
+        ></TextField>
+        <TextField
+          sx={{ width: "300px", marginBottom: "10px" }}
+          variant="outlined"
+          value={first_name}
+          onChange={(event) => {
+            setFirstName(event.target.value);
+          }}
+        ></TextField>
+        <TextField
+          sx={{ width: "300px", marginBottom: "10px" }}
+          variant="outlined"
+          value={last_name}
+          onChange={(event) => {
+            setLastName(event.target.value);
+          }}
+        ></TextField>
+        <TextField
+          sx={{ width: "300px", marginBottom: "10px" }}
+          variant="outlined"
+          disabled
+          value={userData!.role}
+        ></TextField>
+        <Button variant="contained" color="primary" onClick={updateUser}>
+          Update
+        </Button>
+
+        <h1>Penalty points : {userData!.penal_amount}</h1>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default UserProfileComponent;
--- nvasshop/src/service/https/user-service.ts
+++ nvasshop/src/service/https/user-service.ts
@@ -1,12 +1,13 @@
 import api from "../../api";
 
 export interface User {
-    email: string;
-    first_name: string;
-    last_name: string;
-    role: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  role: string;
+  penal_amount: number;
 }
 
 export const getUser = async (id: number): Promise<User> => {
-    return api.get(`/user/${id}`);
+  return api.get(`/user/${id}`);
 };
