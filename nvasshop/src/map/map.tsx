d2738f2a0ed13774a5529af566325eecce9c367d
Breskva <vukmacanovic@gmail.com>
2024-01-31 21:39:23 +0100

    Implemented displaying coords from websocket

--- nvasshop/src/map/map.tsx
+++ nvasshop/src/map/map.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
 import { LatLngTuple, Icon } from 'leaflet';
 import './map.css'; // Import the CSS file
 import "leaflet/dist/leaflet.css";
@@ -7,70 +7,108 @@ import api from '../api';
 
 const MapComponent: React.FC = () => {
 
+    type MarkerType = {
+        geocode: [number, number];
+        popUp: string;
+    };
+
+    const [startEndMarkers, setStartEndMarkers] = useState<MarkerType[]>([]);
+    const [markers, setMarkers] = useState<MarkerType[]>([]);
+    const [updateFrequency, setUpdateFrequency] = useState('1');
+
     useEffect(() => {
         let url = 'ws://localhost:8000/ws/socket-server/';
         const chatSocket = new WebSocket(url);
-    
+
         chatSocket.onmessage = function (e) {
             let data = JSON.parse(e.data);
             console.log('Data: ', data);
-        }
-    
-        chatSocket.onopen = function(e) {
+
+            if (data.coordinates) {
+                let newMarker = {
+                    geocode: data.coordinates, // assuming data.coordinates is an array with two elements
+                    popUp: 'Current position' // replace with the actual popup content
+                };
+                console.log(newMarker)
+                setMarkers([newMarker]);
+            } else {
+                console.error('Coordinates are undefined');
+            }
+        };
+
+        chatSocket.onopen = function (e) {
             console.log('WebSocket is open');
         };
-        
-        chatSocket.onerror = function(error) {
+
+        chatSocket.onerror = function (error) {
             console.log('WebSocket Error: ', error);
+        };
+
+        return () => {
+            chatSocket.close();
         };
     }, []);
 
-    const markers = [
-        {
-            geocode: [48.86, 2.3522],
-            popUp: "Hello, I am pop up 1"
-        },
-        {
-            geocode: [48.85, 2.3522],
-            popUp: "Hello, I am pop up 2"
-        },
-        {
-            geocode: [48.855, 2.34],
-            popUp: "Hello, I am pop up 3"
-        }
-    ]
+    const markerIcon = new Icon({
+        iconUrl: process.env.PUBLIC_URL + '/locationMarker.png',
+        iconSize: [42, 42]
+    })
 
-    const customIcon = new Icon({
-        iconUrl: process.env.PUBLIC_URL + '/mapMarker.png',
+    const truckIcon = new Icon({
+        iconUrl: process.env.PUBLIC_URL + '/truckMarker.png',
         iconSize: [65, 65]
     })
 
-    const handleButtonClick = async() => {
-        try{
-            const response = await api.post(`locationsim/coordinates/`, {
-            }, {
+    const handleButtonClick = async () => {
+        try {
+            const response = await api.post(`locationsim/coordinates/`, {}, {
                 headers: {
                     'Content-Type': 'application/json',
                 },
             });
-        }catch(error){
+            const { start_coordinates, end_coordinates } = response.data;
+            setStartEndMarkers([
+                { geocode: start_coordinates, popUp: 'Start point' },
+                { geocode: end_coordinates, popUp: 'End point' }
+            ]);
+        } catch (error) {
             console.error('Error asking for coordinates: ', error);
             throw error;
         }
     };
 
+    const handleFrequencyChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setUpdateFrequency(event.target.value);
+    };
+
     return (
-        <div>
-            <button style={{margin: '10px'}} onClick={handleButtonClick}>Click me</button>
+        <div className="parent-div">
+            <button onClick={handleButtonClick} className='start-button'>Start delivery</button>
+            <div className="frequency-div">
+                Update frequency:
+                <select className="frequency-select" value={updateFrequency} onChange={handleFrequencyChange}>
+                    <option value="1">1s</option>
+                    <option value="3">3s</option>
+                    <option value="30">30s</option>
+                    <option value="60">1min</option>
+                </select>
+            </div>
             <MapContainer center={[48.8566, 2.3522]} zoom={13} className="map-container" >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {markers.map(marker => (
-                    <Marker position={marker.geocode as LatLngTuple} icon={customIcon}>
+                {startEndMarkers.map(marker => (
+                    <Marker position={marker.geocode as LatLngTuple} icon={markerIcon}>
                         <Popup>
-                            <h4>{marker.popUp}</h4>
+                            <p style={{ fontSize: '1.3em' }}>{marker.popUp}</p>
+                        </Popup>
+                    </Marker>
+                ))}
+                {markers.map(marker => (
+                    <Marker position={marker.geocode as LatLngTuple} icon={truckIcon}>
+                        <Popup>
+                            <p style={{ fontSize: '1.3em' }}>{marker.popUp}</p>
                         </Popup>
                     </Marker>
                 ))}
